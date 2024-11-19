7a3778b1b8ba5807df5bfbc71eb5f6705f9cab1e
tokunaga <js23005@anabuki-net.ne.jp>
2024-11-19 15:11:17 +0900

    2024-11-19 追加 フォーム機能完成 メール送信機能追加中

--- private_diary/diary/forms.py
+++ private_diary/diary/forms.py
@@ -1,15 +1,26 @@
 from django import forms
 
+# Djangoで用意されているメールを送信する機能
+from django.core.mail import EmailMessage
 
+#Pythonクラスで入力フォールのデータを定義している
+#MVCモデルのM(odel)を表している
 class InquiryForm(forms.Form):
+    #フィールド変数の入力項目
     name = forms.CharField(label='お名前', max_length=30)
     email = forms.EmailField(label='メールアドレス')
     title = forms.CharField(label='タイトル', max_length=30)
-    message = forms.CharField(label='メッセージ', widhet=forms.Textarea)
+    message = forms.CharField(label='メッセージ', widget=forms.Textarea)
 
+    # Python クラスのコンストラクター
+    # self   今回のインスタンスを示す変数
     def __init__(self, *args, **kwargs):
+        #　スーパークラスのコンストラクターを呼ぶ
+        # ※forms.Formを継承したクラスでは最初にスーパークラスのコンストラクターを呼ぶこと
         super().__init__(*args, **kwargs)
 
+        # HTMLのエレメントに対して属性を設定する
+        # →コンストラクター内で設定する
         self.fields['name'].widget.attrs['class'] = 'form-control'
         self.fields['name'].widget.attrs['placeholder'] = 'お名前をここに入力してください。'
         self.fields['email'].widget.attrs['class'] = 'form-control'
@@ -18,3 +29,6 @@ class InquiryForm(forms.Form):
         self.fields['title'].widget.attrs['placeholder'] = 'タイトルをここに入力してください。'
         self.fields['message'].widget.attrs['class'] = 'form-control'
         self.fields['message'].widget.attrs['placeholder'] = 'メッセージをここに入力してください。'
+
+    # メール送信メソッドを追加する
+    def send_mail(self) :
--- private_diary/diary/views.py
+++ private_diary/diary/views.py
@@ -1,8 +1,37 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-#from django.views import generic
+from django.views import generic
 from django.views.generic import TemplateView
+
+from .forms import InquiryForm
+
+#お問い合わせ送信内容をログに残す
+import logging
+logger = logging.getLogger(__name__)
+
+# 'アプリ名:url名'からurlを取り出す
+from django.urls import reverse_lazy
+
 # Create your views here.
 #ここにクラスを登録することによってアプリ内の画面を登録する
 
+# IndexViewクラスはindex.htmlをもとにページを構成する
 class IndexView(TemplateView):
     template_name = "index.html"
+
+# お問い合わせのページ
+class InquiryView(generic.FormView):
+    template_name = "inquiry.html"
+    form_class = InquiryForm
+    #フォーム画面を表示するだけならここまで
+    success_url = reverse_lazy('diary:index')
+
+    # メソッドのオーバーライド
+    # フォームに正しい値が入力され送信されたときの処理
+    def form_valid(self, form: Any) -> HttpResponse:
+        # メールを送信する
+        form.send_email()
+        # 送信したことをログに残す
+        logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
+        return super().form_valid(form)
+
