6b1f6377403cba22656e36a47d3a757b4f40733a
tokunaga <js23005@anabuki-net.ne.jp>
2024-12-10 11:15:56 +0900

    Django作成

--- private_diary/accounts/models.py
+++ private_diary/accounts/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+#AbstractUserクラスを継承したCustomUserクラス
 class CustomUser(AbstractUser):
     """ 拡張ユーザーモデル """
 
